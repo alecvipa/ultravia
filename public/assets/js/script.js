7885576447b59a79266322333f91c330ad9e00be
alecvipa <hebrit_626@hotmail.com>
2023-10-06 09:14:44 -0500

    added new image

---
+++ public/assets/js/script.js
@@ -0,0 +1,11 @@
+function navB() {
+    if(window.scrollY==0){
+        //user scrolled to the top of the page
+        document.getElementById("navBars").style.backgroundColor = "" ;
+    }
+    else{
+
+        document.getElementById("navBars").style.backgroundColor = "black" ;
+    }
+    
+}
