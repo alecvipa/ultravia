1de1c3e7f0f8925b2ef68b72e5fad80c1bceed05
alecvipa <hebrit_626@hotmail.com>
2023-10-08 14:43:02 -0400

    finished site, missing privacy policy

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -2,10 +2,15 @@ function navB() {
     if(window.scrollY==0){
         //user scrolled to the top of the page
         document.getElementById("navBars").style.backgroundColor = "" ;
+       
+        
     }
     else{
 
-        document.getElementById("navBars").style.backgroundColor = "black" ;
+        document.getElementById("navBars").style.backgroundColor = "#2153a1 " ;
+       
+        
     }
+    
     
 }
--- server.js
+++ server.js
@@ -64,11 +64,11 @@ app.post("/ajax/email", function (request, response) {
     });
 
 
-    var htmlBody = `<h2>Contacto</h2><p>Nombre: ${request.body.name} </p><p>Apellidos: ${request.body.surename} </p><p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Número de contacto:${request.body.number} </p><p>Mensaje (opcional): ${request.body.message}</p>`;
+    var htmlBody = `<h2>Correo de contacto</h2><p>Nombre: ${request.body.name} </p><p> Correo electrónico: <a href='mailto: ${request.body.email}'>${request.body.email}</a></p><p>Mensaje:${request.body.message} </p>`;
     var mail = {
         from: '"Team: Xyncs Web Studio',
-        to: 'atnclientes@global-moving.com',
-        subject: '¡Alguien ha dejado sus datos en Global Moving!',
+        to: 'hebrit_626@hotmail.com',
+        subject: '¡Alguien ha dejado sus datos en Ultravía Logistics!',
         html: htmlBody
     };
     transporter.sendMail(mail, function (err, info) {
